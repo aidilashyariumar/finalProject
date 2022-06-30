0b91088e35df8b4d650df5acb19687133aa41d73
Tomjerry12345 <aidilashyariumar@gmail.com>
2022-07-01 05:38:15 +0800

    awal

---
+++ index.php
@@ -0,0 +1,272 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+<style>
+  .logo {
+    background-color: #F02C2E;
+    background-image: url(Vector.png);
+    padding: 20px;
+  }
+
+  .navbar {
+    box-shadow: 1px 1px 3px;
+  }
+
+  .jumlah {
+
+    background-image: linear-gradient(to bottom right, #F02C2E, #F87F81);
+    width: 100%;
+    color: white;
+    padding: 100px;
+    text-align: center;
+
+  }
+</style>
+
+<body>
+  <nav class="navbar navbar-expand-lg  fixed-top " style="background-color:white ;">
+    <div class="container">
+      <a class="navbar-brand" href="#"><img src="Group 8.png" alt=""></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto fw-bold">
+          <li class="nav-item me-3">
+            <a class="nav-link" href="index.php">Beranda</a>
+          </li>
+          <li class="nav-item me-3">
+            <a class="nav-link" href="index.php#">Tentang Kami</a>
+          </li>
+          <li class="nav-item me-3">
+            <a class="nav-link ">Penelusuran</a>
+          </li>
+          <button type="button" class="btn btn-outline-danger">Mulai</button>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <section class="container d-flex justify-content-between align-items-center " style="margin-top: 150px;">
+    <div class="container">
+      <div class="row  d-flex justify-content-between align-items-center " style="">
+        <div class="col-12 col-lg-6 col-md-6">
+          <div class="div">
+            <h1 class="fw-bold">Selamat Datang di Berkisar</h1>
+            <p>Berkisar adalah sebuah tempat bagi <br>
+              Masyarakat Sulawesi Selatan untuk <br>
+              berkeluh kesah pada Pemerintah</p>
+          </div>
+        </div>
+        <div class="col-12 col-lg-6 col-md-6 ">
+          <div class="div">
+            <img src="Gambar.png" alt="">
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+
+  <section class="container mt-5">
+    <div class="container">
+      <div class="row  d-flex justify-content-between align-items-center">
+        <div class="col col-12 col-lg-6 col-md-">
+          <div class="img">
+            <img src="Group 27.jpg" alt="">
+          </div>
+        </div>
+        <div class="col col-12 col-lg-6 col-md-">
+          <div class="kritik" style="width:100% ;">
+            <h2>Silahkan mulai berkeluh kesah!</h2>
+            <form>
+              <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+              </div>
+              <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Kritik</label>
+                <textarea type="text" class="form-control" id="exampleInputPassword1"> </textarea>
+              </div>
+              <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Saran</label>
+                <textarea type="text" class="form-control" id="exampleInputPassword1"> </textarea>
+              </div>
+              <div class="mb-3">
+                <label for="Select" class="form-label">Kategori</label>
+                <select id="Select" class="form-select">
+                  <option>lain-lain</option>
+                  <option>banjir</option>
+                  <option>gempa</option>
+                  <option>jalan berlubang</option>
+                </select>
+              </div>
+              <div class="mb-3">
+                <label for="Select" class="form-label">instansi</label>
+                <select id="Select" class="form-select">
+                  <option>Disabled select</option>
+                  <option>Disabled select</option>
+                </select>
+              </div>
+              <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Sembunyikan Edintitas</label>
+              </div>
+              <button type="submit" class="btn btn-danger">KIRIM</button>
+            </form>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+
+  </section>
+
+  <section>
+    <h2 class=" d-flex justify-content-center mt-5">Website Pemerintah Yang Terhubung</h2>
+    <div class="logo mt-4">
+      <div class="container mt-2">
+
+
+        <div class="logo1 d-flex justify-content-around ">
+          <img src="gowa 1.png" alt="">
+          <img src="bone 1.png" alt="">
+          <img src="bulkum 1.png" alt="">
+          <img src="selayar 1.png" alt="">
+          <img src="lutra 1.png" alt="">
+        </div>
+        <div class="logo1 d-flex justify-content-around mt-4">
+          <img src="gowa 1.png" alt="">
+          <img src="bone 1.png" alt="">
+          <img src="bulkum 1.png" alt="">
+          <img src="selayar 1.png" alt="">
+          <img src="lutra 1.png" alt="">
+        </div>
+
+      </div>
+    </div>
+  </section>
+  <!-- proses berkeluh kesah -->
+  <section>
+    <div class="container my-5">
+      <h2 class="fw-bold d-flex justify-content-center my-5">Proses Berkeluh Kesah</h2>
+      <div class="d-flex justify-content-center">
+        <img class="" src="Group 28.png" style="width: 80%;" alt="">
+      </div>
+    </div>
+  </section>
+
+  <section>
+    <div class="jumlah">
+      <h2 class="my-4">Jumlah Aduan Yang Masuk</h2>
+      <h1 class="fw-bold">1000</h1>
+    </div>
+  </section>
+
+  <!-- aduan -->
+  <section class="my-5">
+
+    <div class="container">
+      <h2 class="fw-bold d-flex justify-content-center my-5">Adua Terbaru</h2>
+      <div class="row">
+        <div class="col-12 col-lg-4 col-md-4">
+          <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                content.</p>
+              <a href="#" class="btn btn-danger">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-lg-4 col-md-4">
+        <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                content.</p>
+              <a href="#" class="btn btn-danger">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-lg-4 col-md-4">
+        <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                content.</p>
+              <a href="#" class="btn btn-danger">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </section>
+
+  <!-- penelusuran -->
+<section>
+  <div class="container">
+
+    <h2 class="fw-bold d-flex justify-content-center my-5">PENELUSURAN ADUAN</h2>
+    <form class="" style="" role="search">
+      <input class="form-control me-2" type="search" placeholder="Cari Aduan" aria-label="Search">
+      <button class="btn btn-danger d-flex justify-content-center my-5" style="width: 10% ; margin:0px auto;" type="submit">CARI</button>
+    </form>
+  </div>
+</section>
+  <!-- tenteng kami -->
+  <section>
+    <div class="">
+      <h2 class="d-flex justify-content-center fw-bold">Tentang Kami</h2>
+      <div class="p-1" style="width:75%; margin:0px auto; background-color: #011627; color:white;">
+        <h3 class="d-flex justify-content-center">Berkisar</h3>
+        <p style="text-indent: 50px;">Berkisar (Beri Kritik dan Saran) adalah sebuah wadah berbentuk website yang
+          menjadi tempat untuk masyarakat Sulawesi Selatan di beberapa Kabupaten agar bisa berkeluh kesah tentang hal -
+          hal yang berhubungan dengan kepemerintahan. <br></p>
+        <p style="text-indent: 50px;">Berkisar diciptakan pada tanggal 10 Desember 2021 oleh seseorang yang tidak ingin
+          disebutkan namanya. Awalnya, Berkisar dibuat untuk memenuhi presentasi karya, tapi seiiring berjalannya waktu
+          Berkisar mulai dikembangkan dan menjadi seperti saat ini. <br></p>
+        <p style="text-indent: 50px;">Berkisar diharapkan menjadi salah satu tempat terpercaya untuk masyarakat yang
+          ingin mengeluarkan keluh kesahnya kepada Pemerintah.
+        </p>
+      </div>
+    </div>
+  </section>
+
+  <section>
+  <div class="container">
+  <div class="row">
+    <div class="col">
+      Column
+    </div>
+    <div class="col">
+      Column
+    </div>
+    <div class="col">
+      Column
+    </div>
+  </div>
+</div>
+  </section>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
+  </script>
+</body>
+
+</html>
